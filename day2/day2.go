7bf73ad3436ffa63c9fad507d1a3135fc3506305
Пользователь <denisdugar@gmail.com>
2020-12-11 14:56:01 +0200

    fix issues

--- day2/day2.go
+++ day2/day2.go
@@ -1,11 +1,10 @@
 package main
 
-import "projects/day2/unique"
+import "projects/day1/day2/unique"
 
 func main() {
 	a := []int{1, 1, 1, 1, 2, 2, 2}
-	var b []int
-	b = unique.UniqueArr(a)
+	var b = unique.UniqueArr(a)
 	for i := 0; i < len(b); i++ {
 		print(b[i])
 	}
--- day2/unique/unique.go
+++ day2/unique/unique.go
@@ -4,23 +4,13 @@ func UniqueArr(arr []int) []int {
 	if len(arr) == 0 {
 		return arr
 	}
-	mass := []int{0}
-	var uniq []int
-	var equal bool = false
-	for i := 0; i < len(arr); i++ {
-		for j := 0; j < len(mass); j++ {
-			if arr[i] == mass[j] {
-				equal = true
-			}
-		}
-		if equal == false {
-			mass = append(mass, arr[i])
-		}
-		if equal == true {
-			equal = false
+	mass := []int{}
+	test := make(map[int]bool)
+	for _, num := range arr {
+		if _, value := test[num]; !value {
+			test[num] = true
+			mass = append(mass, num)
 		}
 	}
-	uniq = append(mass[:0], mass[1:]...)
-	return uniq
-
+	return mass
 }
