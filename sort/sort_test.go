fb62c4b765eaea0f50c5a98cdd46a924d764e657
Пользователь <denisdugar@gmail.com>
2020-12-11 12:17:56 +0200

    fix issues1

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -7,30 +7,36 @@ import (
 )
 
 var testCases = []struct {
+	name     string
 	actual   []int
 	expected []int
 }{
-	{actual: []int{5, 7, 3, 8, 1, 9, 0, 2, 5, 10}, expected: []int{0, 1, 2, 3, 5, 5, 7, 8, 9, 10}},
-	{actual: []int{1, 1, 1, 1, 1, 1, 1, 1}, expected: []int{1, 1, 1, 1, 1, 1, 1, 1}},
-	{actual: []int{1}, expected: []int{1}},
-	{actual: []int{}, expected: []int{}},
-	{actual: []int{1, 2, 3, 4, 5}, expected: []int{1, 2, 3, 4, 5}},
+	{actual: []int{5, 7, 3, 8, 1, 9, 0, 2, 5, 10}, expected: []int{0, 1, 2, 3, 5, 5, 7, 8, 9, 10}, name: "simple sort slice"},
+	{actual: []int{1, 1, 1, 1, 1, 1, 1, 1}, expected: []int{1, 1, 1, 1, 1, 1, 1, 1}, name: "sort equal"},
+	{actual: []int{1}, expected: []int{1}, name: "sort one"},
+	{actual: []int{}, expected: []int{}, name: "sort empty"},
+	{name: "sort nil"},
+	{actual: []int{1, 2, 3, 4, 5}, expected: []int{1, 2, 3, 4, 5}, name: "sort sorted slice"},
 }
 
-func TestFlagParser(t *testing.T) {
-	var a []int
-	var b []int
+func TestSortMy(t *testing.T) {
 	for _, tt := range testCases {
-		t.Run("test", func(t *testing.T) {
-			a = tt.actual
-			SortT(a)
-			b = tt.actual
-			sort.Ints(b)
-			if !reflect.DeepEqual(a, tt.expected) {
-				t.Errorf("got %q, want %q", a, tt.expected)
+		t.Run(tt.name, func(t *testing.T) {
+			SortT(tt.actual)
+			if !reflect.DeepEqual(tt.actual, tt.expected) {
+				t.Errorf("got %q, want %q", tt.actual, tt.expected)
 			}
-			if !reflect.DeepEqual(b, tt.expected) {
-				t.Errorf("got %q, want %q", b, tt.expected)
+
+		})
+	}
+}
+func TestSortGo(t *testing.T) {
+	for _, tt := range testCases {
+		t.Run("TestSortGo", func(t *testing.T) {
+			sort.Ints(tt.actual)
+
+			if !reflect.DeepEqual(tt.actual, tt.expected) {
+				t.Errorf("got %q, want %q", tt.actual, tt.expected)
 			}
 		})
 	}
